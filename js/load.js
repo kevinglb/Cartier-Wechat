888c53035385a6102e35c0f2d109e702cec2e899
Kevin <kevinglbx@gmail.com>
2016-06-19 10:25:22 +0800

    upload files

---
+++ js/load.js
@@ -0,0 +1,319 @@
+(function(){
+	var circle = document.getElementById('loadCircle'),
+		loadContainer = document.getElementsByClassName('loadContainer')[0],
+		loadWrap = document.getElementsByClassName('loadWrap')[0],
+		ctx = circle.getContext('2d'),
+		cx = circle.width/2,
+		cy = circle.height/2,
+		num = 0,//图片计数器
+		numLength = 50,//图片总数量
+		percentage = document.getElementsByClassName('loadNum')[0],
+		spinner = document.getElementsByClassName('loadSpinner')[0],
+	    overlay = document.getElementsByClassName('overlay')[0],
+        navi = document.getElementsByClassName('navi')[0],
+		a = 0;
+
+	// var b=setInterval(function(){
+	// 	if (num<=numLength) {//百分比
+	// 		a = num/numLength * 300;//310为最后弧度
+	// 		percentage.innerHTML = parseInt(num/numLength*100) + '%'
+	// 		ctx.beginPath();
+	// 		ctx.arc(cx,cy,circle.width*0.47, -45*Math.PI/180, a*Math.PI/180);
+	// 		ctx.lineWidth = 2;
+	// 		ctx.strokeStyle="#fff";
+	// 		ctx.stroke();//圆
+	// 		ctx.closePath();
+	// 		num++;
+	// 	}else {
+	// 	    window.clearInterval(b);
+ //            b = 0;
+ //            //swith the cavas circle with the img one
+ //            circle.style.display = 'none';
+ //            spinner.style.display = 'block';
+
+ //            if(!loadContainer.classList.contains('expand')){
+ //                loadContainer.classList += ' expand';
+ //            } 
+ //            percentage.style.display = 'none';
+ //            if(!circle.classList.contains('expand')){
+ //                circle.classList += ' expand';
+ //            } 
+ //            window.setTimeout(function(){
+ //                if(!spinner.classList.contains('rotating')){
+ //                    spinner.classList += ' rotating';
+ //                }
+ //            },500);
+	//    }
+ //    },20);
+	
+	ctx.beginPath();
+	ctx.strokeStyle="#fff"; 
+	ctx.lineWidth = 2; 
+	ctx.moveTo(circle.width*0.78 , circle.height*0.19);//戒指横线
+	ctx.lineTo(circle.width*0.87 , circle.height*0.1);
+	ctx.stroke();//横线
+	ctx.closePath();
+	loadImg();
+	function loadImg(){
+		var imgArr = ['img/jpg/360_f.jpg','img/jpg/360_l.jpg','img/jpg/360_b.jpg','img/jpg/360_d.jpg','img/jpg/360_u.jpg','img/jpg/360_r.jpg',
+                      'img/png/glass-bg.png','img/jpg/index-bg.jpg','img/jpg/menu-bg.jpg','img/png/title-film.png','img/png/title-bonus.png','img/png/title-luhan.png','img/png/title-lisa.png','img/png/title-liqi.png',
+					  'img/png/title-gallery.png','img/png/title-shop.png','img/jpg/menu-bonus.jpg','img/jpg/menu-film.jpg','img/jpg/menu-luhan.jpg',
+					  'img/jpg/menu-lisa.jpg','img/jpg/menu-liqi.jpg','img/jpg/menu-gallery.jpg','img/jpg/menu-shop.jpg','img/jpg/luhan-1.jpg','img/jpg/luhan-2.jpg',
+                      'img/jpg/luhan-3.jpg','img/jpg/luhan-4.jpg','img/jpg/luhan-5.jpg'];
+		var num = 0,
+			len = imgArr.length;
+		for(var i=0;i<imgArr.length;i++){
+            var image = new Image();
+            image.src = imgArr[i];
+            //imgArr[i].image= image;
+            image.onload = function(){
+                num++;
+                updateLoader(num,len);
+            };
+        }
+    }
+    function updateLoader(num,len){
+        if(num){
+                // var b=setInterval(function(num){
+                    
+					if (num!=len) {//百分比
+						a = num/len*290;//310为最后弧度
+						index = parseInt(num/len*100);
+						percentage.innerHTML = index + '%'
+						ctx.beginPath();
+						ctx.arc(cx,cy,circle.width*0.47, -45*Math.PI/180, a*Math.PI/180);
+						ctx.lineWidth = 2;
+						ctx.strokeStyle="#fff";
+						ctx.stroke();//圆
+						ctx.closePath();
+					}else{
+					   //clear the interval
+						// window.clearInterval(b);
+						// b = 0;
+						//swith the cavas circle with the img one
+						circle.style.display = 'none';
+						spinner.style.display = 'block';
+
+						if(!loadContainer.classList.contains('expand')){
+							loadContainer.classList += ' expand';
+						} 
+						percentage.style.display = 'none';
+						if(!circle.classList.contains('expand')){
+							circle.classList += ' expand';
+						} 
+						window.setTimeout(function(){
+						    if(!spinner.classList.contains('rotating')){
+							 spinner.classList += ' rotating';
+							}
+						},500);
+					}
+				// },20);
+                // if(num >= imgArr.length && typeof callback == "function"){
+                //     //console.log(callback);
+                //     callback(); //the default 'this' in callback will refer to window scoop
+                // };
+            }
+        }
+	
+    
+	//variables for glass effect
+	const TWO_PI = Math.PI * 2;
+	var images = [], imageIndex = 0;
+	var image;
+	var vertices = [], indices = [], fragments = [];
+	var container = document.getElementById('container');
+	var imageWidth = window.innerWidth, imageHeight = window.innerHeight;
+	var clickPosition = [imageWidth * 0.5,imageHeight * 0.5];
+	window.onload = function(){
+    //TweenMax.set(container, { perspective: 500 });
+    	var urls = ['img/png/glass-bg.png'],
+    	 	image, loaded = 0;
+    	images[0] = image = new Image();
+    	image.onload = function(){
+            	
+           if (++loaded === 1) {
+            imagesLoaded();
+            for (var i = 1; i < 4; i++) {
+                // if (window.CP.shouldStopExecution(1)) {
+                //     break;
+                // }
+                images[i] = image = new Image();
+                image.src = urls[i];
+            }
+        }		
+    	};
+    	image.src = urls[0];
+	};
+
+	function imagesLoaded() {
+    	placeImage(false);
+   	 	triangulate();
+        
+    	//break the glass
+    	//shatter();
+	}
+	//place the image
+	function placeImage(transitionIn) {
+    	image = images[imageIndex];
+    	if (++imageIndex === images.length)
+        imageIndex = 0;
+    	loadWrap.addEventListener('click', imageClickHandler);
+        image.style.opacity = 0;
+        container.appendChild(image);
+    }
+    function imageClickHandler(event) {
+    	var box = loadWrap.getBoundingClientRect(), top = box.top, left = box.left;
+    	clickPosition[0] = event.clientX - left;
+    	clickPosition[1] = event.clientY - top;
+        image.style.opacity = 1;
+    	triangulate();
+
+        window.setTimeout(function(){
+            shatter();
+            loadWrap.style.background = 'transparent';
+        },500);
+    	//tracking code
+        validTracking('Experience');
+	}
+	function triangulate() {
+    	var rings = [{r: 80,c: 12},{r:100,c:20},{r: 150,c: 12},,{r: 300,c: 12},{r: 1200,c: 12}],
+        	x, y, centerX = clickPosition[0], centerY = clickPosition[1];
+    	vertices.push([centerX,centerY]);
+    	rings.forEach(function (ring) {
+        	var radius = ring.r, count = ring.c, variance = radius * 0.25;
+        	for (var i = 0; i < count; i++) {
+	            x = Math.cos(i / count * TWO_PI) * radius + centerX + randomRange(-variance, variance);
+    	        y = Math.sin(i / count * TWO_PI) * radius + centerY + randomRange(-variance, variance);
+        	    vertices.push([x,y]);
+ 	       }
+    	});
+    	vertices.forEach(function (v) {
+        	v[0] = clamp(v[0], 0, imageWidth);
+        	v[1] = clamp(v[1], 0, imageHeight);
+   	 	});
+    	//console.log(vertices);
+    	indices = Delaunay.triangulate(vertices);
+    	//console.log(indices);
+	}
+	function shatter() {
+    	var p0, p1, p2, fragment;
+    	var tl0 = new TimelineMax({ onComplete: shatterCompleteHandler });
+
+        loadContainer.classList += ' dn';
+   	 	for (var i = 0; i < indices.length; i += 3) {
+       		p0 = vertices[indices[i + 0]];
+        	p1 = vertices[indices[i + 1]];
+        	p2 = vertices[indices[i + 2]];
+        	fragment = new Fragment(p0, p1, p2);
+        	var dx = fragment.centroid[0] - clickPosition[0], dy = fragment.centroid[1] - clickPosition[1], d = Math.sqrt(dx * dx + dy * dy), rx = 300 * sign(dy), ry = 900 * -sign(dx);
+        	//, delay = d * 0.003 * randomRange(0.1, 0.25)
+        	fragment.canvas.style.zIndex = Math.floor(d).toString();
+        	var tl1 = new TimelineMax();
+        	tl1.to(fragment.canvas, randomRange(0.2,1.5), {
+           		z: randomRange(-1500, 1500),
+            	rotationX: rx,
+            	rotationY: ry,
+            	x: randomRange(-2000, 2000),
+            	y: randomRange(-2000, 2000),
+            	ease: Expo.easeInOut
+        	});
+        	tl1.to(fragment.canvas, 0.4, { alpha: 0 }, 0.6);
+        	tl0.insert(tl1, 0.1);
+        	fragments.push(fragment);
+        	container.appendChild(fragment.canvas);
+    	}
+
+        if(overlay.classList.contains('dn')){
+            overlay.classList.remove('dn');
+        }
+        if(navi.classList.contains('dn')){
+            navi.classList.remove('dn');
+        }
+        container.removeChild(image);
+        loadWrap.removeEventListener('click', imageClickHandler);
+        
+        window.setTimeout(function(){
+            loadWrap.classList.add('dn');
+            overlay.classList.add('fadeOut');
+            navi.classList.add('fadeOut');
+            loadcomplete();
+        },2500);
+    	
+        window.setTimeout(function(){
+            overlay.classList.remove('fadeOut');
+            overlay.classList.add('dn');
+            navi.classList.remove('fadeOut');
+            navi.classList.add('dn');
+            overlay.addEventListener('click',toggleMenu);
+
+        },4500);
+        
+       
+	}
+	function shatterCompleteHandler() {
+    	fragments.forEach(function (f) {
+        	container.removeChild(f.canvas);
+    	});
+    	fragments.length = 0;
+    	vertices.length = 0;
+    	indices.length = 0;
+    	//placeImage();
+	}
+	function randomRange(min, max) {
+    	return min + (max - min) * Math.random();
+	}
+	function clamp(x, min, max) {
+    	return x < min ? min : x > max ? max : x;
+	}
+	function sign(x) {
+    	return x < 0 ? -1 : 1;
+	}
+	//fragment for glass
+	Fragment = function (v0, v1, v2) {
+    	this.v0 = v0;
+    	this.v1 = v1;
+    	this.v2 = v2;
+    	this.computeBoundingBox();
+    	this.computeCentroid();
+    	this.createCanvas();
+    	this.clip();
+	};
+	Fragment.prototype = {
+    	computeBoundingBox: function () {
+        	var xMin = Math.min(this.v0[0], this.v1[0], this.v2[0]), xMax = Math.max(this.v0[0], this.v1[0], this.v2[0]), yMin = Math.min(this.v0[1], this.v1[1], this.v2[1]), yMax = Math.max(this.v0[1], this.v1[1], this.v2[1]);
+        	this.box = {
+            	x: xMin,
+            	y: yMin,
+            	w: xMax - xMin,
+            	h: yMax - yMin
+        	};
+    	},
+    	computeCentroid: function () {
+        	var x = (this.v0[0] + this.v1[0] + this.v2[0]) / 3, y = (this.v0[1] + this.v1[1] + this.v2[1]) / 3;
+        	this.centroid = [x,y];
+   		},
+    	createCanvas: function () {
+        	this.canvas = document.createElement('canvas');
+        	this.canvas.width = this.box.w;
+        	this.canvas.height = this.box.h;
+        	this.canvas.style.width = this.box.w + 'px';
+        	this.canvas.style.height = this.box.h + 'px';
+        	this.canvas.style.left = this.box.x + 'px';
+        	this.canvas.style.top = this.box.y + 'px';
+        	this.ctx = this.canvas.getContext('2d');
+    	},
+    	clip: function () {
+        	this.ctx.translate(-this.box.x, -this.box.y);
+        	this.ctx.beginPath();
+       		this.ctx.moveTo(this.v0[0], this.v0[1]);
+        	this.ctx.lineTo(this.v1[0], this.v1[1]);
+        	this.ctx.lineTo(this.v2[0], this.v2[1]);
+        	this.ctx.closePath();
+        	this.ctx.clip();
+        	this.ctx.drawImage(image, 0, 0);
+    	}
+	};
+
+
+	
+}());
---
+++ js/main.js
@@ -0,0 +1,236 @@
+
+	var videoData = {'film':{'titleImg': 'img/png/title-film.png','text':'卡地亚诚邀您欣赏我们的全新品牌宣传大片，\n一起沉浸在Juste Un Clou的世界','src':'http://v.qq.com/iframe/player.html?vid=q0307i6fbn9&amp;'},
+					 'bonus':{'titleImg': 'img/png/title-bonus.png','text':'揭开幕后知错过程。\n观看导演、演员、摄影师的访谈','src':''},
+					 'luhan':{'titleImg': 'img/png/title-luhan.png','text':'男演员、歌手鹿晗年纪轻轻就展现多才多艺的才华，他以唱歌跳舞来\n表现自我，对于表演的热情从来不曾消减。他举手土族之间展现出\n自然不扭捏的自信，他敢于追求自己的梦想，从不停下他的脚步。','src':'http://v.qq.com/iframe/player.html?vid=p03071r5vbp&amp;'},
+					 'lisa':{'titleImg': 'img/png/title-lisa.png','text':'时尚导演Lisa Paclet，她擅长完美的结合舞蹈、时尚、艺术在她的影\n像之中，她浪漫的性格与精准的肢体动作指导使得这次影片达到一个\n尽善尽美的视觉飨宴。','src':'http://v.qq.com/iframe/player.html?vid=s0307k3wmkb&amp;'},
+					 'liqi':{'titleImg': 'img/png/title-liqi.png','text':'时尚摄影师李奇，他在黑白摄影中博主哦永垂不朽的经典。通过他的镜\n头，每一位大明星总能自在的展现本真的自我。李奇不造作，坦荡荡\n的艺术家气息完全符合Juste un Clou精神。','src':''}};
+
+	var shopData = {};
+
+	//initVideoWrap, initShopWrap and initGalleryWrap should run after load.js has completed
+	//initVideo is dynamic it is not called at beginning
+	initHeadBar();
+	window.setTimeout(function(){
+		initMenu();
+	},200);	
+	initShopWrap();
+	initGalleryWrap();
+	initMusic();
+
+	//init the menu by assigning the img src and bind click functions
+	function initMenu(){
+		//var $_linkArr = $('.menu .menuLink');
+		var height = window.innerHeight,
+      		rem = document.getElementsByTagName('html')[0].style['font-size'].slice(0, -2),
+      		maxHeight = height - 1.6 * parseInt(rem);
+
+		$('.menu .menuLink').each(function(index,item){
+			$(this).find('img').attr('src','img/jpg/menu-'+this.getAttribute('data-para')+'.jpg');
+			$(this).bind('click',function(){
+				showWrap(this.getAttribute('data-type'),this.getAttribute('data-para'));
+				validTracking('Menu-'+this.getAttribute('data-track'));
+			});
+		});
+
+      	if($('.menu').height() < maxHeight){
+      		$('.menu').css('min-height', maxHeight+'px');
+      	}else{
+      		$('.menu').addClass('oversize');
+      		$('.menu').css('max-height', maxHeight+'px');
+      	}
+	}
+
+	//initHeadBar img and click function 
+	function initHeadBar(){
+		var $_menuBtn = $('.headBar .menuBtn'),
+			$_musicBtn = $('.headBar .musicBtn'),
+			$_closeBtn = $('.headBar .closeBtn');
+			$_headImg = $('.headBar .headImg');
+		//assign the img src
+		$_menuBtn.find('img').attr('src','img/png/btn-menu.png');
+		$_musicBtn.find('img').attr('src','img/png/btn-music.png');
+		$_closeBtn.find('img').attr('src','img/png/btn-close.png');
+		$_headImg.attr('src','img/png/head-logo.png');
+
+		$_menuBtn.bind('click',function(){
+			toggleMenu();
+			validTracking('Menu');
+		});
+		$_musicBtn.bind('click',function(){
+		 	toggleMusic(this);
+		 	validTracking('Menu-Music');
+		});
+		$_closeBtn.bind('click',function(){
+
+			$('.videoContainer .videoFrame').attr('src','');
+		    toggleWrap(this.getAttribute('data-target'));
+		});
+	}
+
+	function initMusic(){
+		// var audio = new Audio('Cartier-bg-music.mp3');
+		// audio.setAttribute('id','audio');
+		// audio.play();
+	}
+
+	function showWrap(target,data){
+		switch(target){
+			case 'video':
+				toggleMenu();
+
+				initVideoWrap(data);
+				break;
+			case 'gallery':
+				toggleMenu();
+				//initGalleryWrap(data);
+				toggleWrap(target);
+				break;
+			case 'shop':
+				toggleMenu();
+				toggleWrap(target);
+				break;
+			default:
+				break;
+		}
+		return;
+	}
+
+	function toggleMenu(){
+      	$('.menu').toggleClass('visible');
+      	$('.overlay').toggleClass('dn');
+    }
+
+    function toggleMusic(musicBtn){
+    	var audio = document.getElementById('audio');
+ 		if(!$(musicBtn).hasClass('off')){
+ 			$(musicBtn).find('img').attr('src','img/png/btn-music-mute.png');
+ 			audio.pause();
+ 		}else{
+ 			$(musicBtn).find('img').attr('src','img/png/btn-music.png');
+ 			audio.play();
+ 		}
+ 		$(musicBtn).toggleClass('off');
+    }
+
+	function toggleWrap(target){
+		$("."+target+"Wrap").toggleClass('visible');
+		
+		if(target === 'gallery' && $('.gallery-container').hasClass('zoomIn')){
+			//if the gallery swiper is visible then reset the gallery by hide the swiper
+			window.setTimeout(function(){
+				$('.galleryContent').removeClass('zoomOut');
+				$('.gallery-container').removeClass('zoomIn').addClass('dn');
+				$('.galleryContainer .swiper-btn').addClass('dn');
+			},400);
+		}
+	}
+
+	function initVideoWrap(data){
+		var obj = videoData[data];
+		var w = $('.videoContainer').width(),
+				h = w*0.8;
+		//pause music when view the video
+		var audio = document.getElementById('audio');
+ 		if(!$('.headBar .musicBtn').hasClass('off')){
+ 			$('.headBar .musicBtn').find('img').attr('src','img/png/btn-music-mute.png');
+ 			audio.pause();
+ 		}
+		$('.videoContainer .titleImg').attr('src',obj['titleImg']);
+		$('.videoContainer .content').text(obj['text']);
+		$('.videoContainer .videoFrame').css('height',h);
+		$('.videoContainer .videoFrame').attr('src',obj['src']+'width='+w+'&amp;height='+h+'&amp;auto=0');
+		toggleWrap('video');
+	}
+
+	function initShopWrap(data){
+		if(!($('.shopWrap').hasClass('initialized'))){
+			$('.shopWrap .titleImg').attr('src','img/png/title-shop.png');
+			initShopSwiper();
+			$('.shopWrap').addClass('initialized');
+		}
+	}
+
+	function initGalleryWrap(data){
+		if(!($('.galleryWrap').hasClass('initialized'))){
+			$('.galleryWrap .titleImg').attr('src','img/png/title-gallery.png');
+			initGallerySwiper();
+			$('.galleryWrap').addClass('initialized');
+		}
+	}
+
+	function initShopSwiper(data){
+		//store brief in array, update after transitionEnd
+		var briefArr = ['Juste un Clou戒指\n18K玫瑰金,钻石','Juste un Clou戒指\r\n白18K金,钻石',
+						'Juste un Clou戒指\r\n18K黄金,钻石','Juste un Clou戒指\n18K金,钻石','Juste un Clou戒指\n18K玫瑰金,钻石',
+						'Juste un Clou戒指\n18K玫瑰金','Juste un Clou戒指\n18K玫瑰金','Juste un Clou手镯\n白18K金,钻石','Juste un Clou手镯\n18K玫瑰金,钻石',
+						'Juste un Clou项链\n18K玫瑰金,钻石','Juste un Clou项链\n白18K金,钻石','Juste un Clou项链\n18K黄金,钻石'];
+		//store ring info in Array, update after transitionEnd
+		var infoArr = ['表达自信不羁的独立精神，彰显个人独特自我。问世于上世纪七十年代纽约的Juste un Clou，体现张扬摩登，特立独行的纽约设计风格，一经推出就广受追捧。作品线条鲜明，将现代气息与大胆创意融为一体。'];
+
+		var updateInfo = function(){
+			var index = $(".ring-container .swiper-slide-active").attr('data-ring');
+			$(".ring-brief").text(briefArr[index]);
+			$(".ring-info").text(infoArr[0]);
+		}
+
+		var ringSwiper = new Swiper('.ring-container',{watchSlidesProgress : true,shortSwipes : true,prevButton:'.shop-button-prev',
+nextButton:'.shop-button-next',slidesPerView : 3,slidesPerGroup : 1,centeredSlides : true, onTransitionEnd:function(swiper){updateInfo()}});
+		$('.ring-container .swiper-slide').each(function(index,item){
+			$(this).find('img').attr('src','img/ring/'+(parseInt(this.getAttribute('data-ring'))+1)+'.png');
+		});
+		updateInfo();
+	}
+
+	function initGallerySwiper(){
+		var gallerySwiper = new Swiper('.gallery-container',{watchSlidesProgress : true,prevButton:'.swiper-button-prev',
+			nextButton:'.swiper-button-next'});
+
+
+		var toggleSwiper = function(target){
+			if($('.gallery-container').hasClass('dn')){
+				//if the swiper is invisible
+				gallerySwiper.slideTo(target,50,false);
+				$('.galleryContent').addClass('zoomOut');
+				$('.gallery-container').addClass('zoomIn').removeClass('dn');
+				$('.galleryContainer .swiper-btn').removeClass('dn');
+			}else{
+				return;
+			}
+		}
+		$('.galleryContent .galleryImg').each(function(index,item){
+			$(this).css('background-image','url(img/jpg/luhan-'+(parseInt(this.getAttribute('data-target'))+1)+'.jpg)');
+			$(this).bind('click',function(){
+				return toggleSwiper(this.getAttribute('data-target'));
+			});
+		});
+
+		$('.gallery-container .swiper-slide').each(function(index,item){
+			$(this).find('img').attr('src','img/jpg/luhan-'+this.getAttribute('data-img')+'.jpg');
+			
+		});
+
+		$('.gallery-container').addClass('dn');
+		$('.galleryContainer .swiper-btn').addClass('dn');	
+	}
+
+	function hostPointClick(target,data,hostname){
+		//_s.cookie.set("hostname",hostname);
+		switch(target){
+			case 'video':
+				initVideoWrap(data);
+				break;
+			case 'gallery':
+				toggleWrap(target);
+				break;
+			case 'shop':
+				toggleWrap(target);
+				break;
+			default:
+				break;
+		}
+		return;
+	}
+
+	function validTracking(eventName){
+		ga('360',eventName);
+	}
